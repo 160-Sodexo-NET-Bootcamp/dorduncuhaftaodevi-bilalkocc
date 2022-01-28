65a8d45920c50348ab48a1d977e5dc8ff04f73c8
Bilal <bilal.kocc@gmail.com>
2022-01-28 22:23:21 +0300

    completed

--- dorduncuodev-bilal-koc/Schedule/JobManager/CakeJobManager.cs
+++ dorduncuodev-bilal-koc/Schedule/JobManager/CakeJobManager.cs
@@ -10,14 +10,14 @@ namespace Schedule.JobManager
 {
     public static class CakeJobManager
     {
-        public static void MakeCake()
+        public static void MakeCake()//her 15dakikada 1 nesne üretimi yapan method
         {
-            RecurringJob.AddOrUpdate<CakeJobs>(x => x.MakingCake(), "*/1 * * * *");
+            RecurringJob.AddOrUpdate<CakeJobs>(x => x.MakingCake(), "*/15 * * * *");
         }
 
-        public static void ChangeStat()
+        public static void ChangeStat()//18:00'da hazırlanıyor durumundan satılabilir durumuna geçiren method
         {
-            RecurringJob.AddOrUpdate<CakeJobs>(x => x.ChangeStatus(), "46 19 * * *" ,TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate<CakeJobs>(x => x.ChangeStatus(), "00 18 * * *" ,TimeZoneInfo.Local);
         }
     }
 }
